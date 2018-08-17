ce617b70466f65ca7ef7a4e7c8d61bc4e0f14874
Unknown <henrique.thsantos.ths@gmail.com>
2018-08-17 16:07:35 -0300

    A simple test with a passing a object as argument
    A class called Employee as argument two objects of the Type Date are passed.

---
+++ src/com/codedemonbr/Employee.java
@@ -0,0 +1,22 @@
+package com.codedemonbr;
+
+public class Employee
+{
+    private String firstName;
+    private String lastName;
+    private Date birthDate;
+    private Date hireDate;
+
+    public Employee (String firstName, String lastName, Date birthDate, Date hireDate)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+        this.hireDate = hireDate;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s, %s Hired: %s Birthday: %s", firstName, lastName, hireDate, birthDate);
+    }
+}
